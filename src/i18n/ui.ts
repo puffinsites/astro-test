6e1909dde3f6752960b5e425385276ebd144f09c
Carlos Pajuelo <carlos.pajuelo.fernandez@gmail.com>
2024-08-16 21:55:43 +0200

    add i18n support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'index.home': '¡Welcome to ',
+      'web.language': 'Languaje: ',
+    },
+    es: {
+      'index.home': '¡Bienvenido a ',
+      'web.language': 'Idioma: ',
+    },
+  } as const;
