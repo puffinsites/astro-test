efe5b902287e1075597815662d7b0fe7b1a0efa8
Carlos Pajuelo <carlos.pajuelo.fernandez@gmail.com>
2024-08-19 12:46:52 +0200

    Cambiar idiomas a banderas

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,17 +3,13 @@ export const languages = {
     es: 'Español',
   };
   
-  export const defaultLang = 'en';
+  export const defaultLang = 'es';
   
   export const ui = {
     en: {
-      'index.home': '¡Welcome to ',
-      'web.language': 'Languaje: ',
       'footer.derechos': 'All right reserved'
     },
     es: {
-      'index.home': '¡Bienvenido a ',
-      'web.language': 'Idioma: ',
       'footer.derechos': 'Todos los derechos reservados'
     },
   } as const;
